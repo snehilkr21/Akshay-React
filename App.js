fdb23bf8adea6981d1ef0f5f8abf76b8aec0f956
snehil <snehil@snehils-MacBook-Air.local>
2023-01-12 00:27:59 +0530

    shifting script file to App.js file

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading1 = React.createElement("h1",{},"heading1")
+        const heading2 = React.createElement("h2",{},"heading2")
+        const container = React.createElement("div",{},[heading1,heading2])
+        const root = ReactDOM.createRoot(document.getElementById("root"))
+        root.render(container)
